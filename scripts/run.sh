7dc0c7223be5e6adf770903ed281993fbcda4480
Sebastian Baltes <research@sbaltes.com>
2017-06-29 15:26:00 +0200

    Updated version number also in bash script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -jar ../target/GitLogParser-0.1-jar-with-dependencies.jar -i /media/data/logs/ -o /media/data2/parsed-logs/ > output.log 2>&1
+java -jar ../target/GitLogParser-1.1-jar-with-dependencies.jar -i /media/data/logs/ -o /media/data2/parsed-logs/ > output.log 2>&1
 
