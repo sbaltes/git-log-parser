08816b19f45c37a7673a350f75bcb7fd2c528355
Sebastian Baltes <research@sbaltes.com>
2018-02-06 08:58:37 +0100

    Update path in run script (new release)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -jar ../target/GitLogParser-1.1-jar-with-dependencies.jar -i /media/data/logs/ -o /media/data2/parsed-logs/ -f "rb java" > output.log 2>&1
+java -jar ../releases/de/unitrier/st/gitlogparser/1.1/gitlogparser-1.1-jar-with-dependencies.jar -i /media/data/logs/ -o /media/data2/parsed-logs/ -f "rb java" > output.log 2>&1
 
